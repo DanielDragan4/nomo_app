715ced48d98736fbc88f585cb8da7f2eea73d553
person5555 <danmartynovich@gmail.com>
2023-12-30 21:19:30 -0500

    commit

--- lib/screens/new_event_screen.dart
+++ lib/screens/new_event_screen.dart
@@ -1,12 +1,35 @@
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 
-class NewEventScreen extends StatelessWidget {
+class NewEventScreen extends StatefulWidget {
   const NewEventScreen({super.key});
 
   @override
+  State<NewEventScreen> createState() => _NewEventScreenState();
+}
+
+class _NewEventScreenState extends State<NewEventScreen> {
+  @override
   Widget build(BuildContext context) {
+    final ImagePicker picker = new ImagePicker();
     return Scaffold(
-      body: const Text("New Event"),
-    );
+        appBar: AppBar(
+          toolbarHeight: 15,
+          titleTextStyle: Theme.of(context).appBarTheme.titleTextStyle,
+          title: Center(
+            child: Text(
+              'Nomo',
+              style: TextStyle(
+                color: Theme.of(context).primaryColor,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+          ),
+        ),
+        body: Column(
+          children: [
+            Text("Create New Event+"),
+          ],
+        ));
   }
 }

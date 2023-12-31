a8408681ddc04d4d9798ce582d280a9355030792
person5555 <danmartynovich@gmail.com>
2023-12-30 22:32:32 -0500

    end me

--- lib/screens/new_event_screen.dart
+++ lib/screens/new_event_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:nomo/widgets/pick_image.dart';
+import 'dart:io';
 
 class NewEventScreen extends StatefulWidget {
   const NewEventScreen({super.key});
@@ -9,9 +10,11 @@ class NewEventScreen extends StatefulWidget {
 }
 
 class _NewEventScreenState extends State<NewEventScreen> {
+  File? _selectedImage;
+  String dropdownValue = "";
+
   @override
   Widget build(BuildContext context) {
-    final ImagePicker picker = new ImagePicker();
     return Scaffold(
         appBar: AppBar(
           toolbarHeight: 15,
@@ -28,7 +31,97 @@ class _NewEventScreenState extends State<NewEventScreen> {
         ),
         body: Column(
           children: [
-            Text("Create New Event+"),
+            const SizedBox(height: 20),
+            const Center(
+              child: Text("Create New Event+"),
+            ),
+            const SizedBox(height: 20),
+            ImageInput(
+              onPickImage: (image) {
+                _selectedImage = image;
+              },
+            ),
+            const SizedBox(height: 10),
+            Column(
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      "Date ",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Calendar Dropdown",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      "Time",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "10:00pm",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ),
+                    const Text("-"),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "12:30am",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      "Location",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Location Picker Here",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ),
+                  ],
+                ),
+                Row(
+                  children: [
+                    const Text(
+                      "Invitation Type",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text(
+                        "Type Dropdown Here",
+                        style: TextStyle(fontSize: 15),
+                      ),
+                    ),
+                  ],
+                ),
+                const Row(
+                  children: [
+                    Text(
+                      "Description",
+                      style: TextStyle(fontSize: 15),
+                    ),
+                  ],
+                ),
+              ],
+            )
           ],
         ));
   }

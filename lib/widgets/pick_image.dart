53853c4a017ffd2e3c54a41be54c3e085f0fa5dc
person5555 <danmartynovich@gmail.com>
2024-03-09 20:04:26 -0500

    Image Upload works. TODO: make unique file each

--- lib/widgets/pick_image.dart
+++ lib/widgets/pick_image.dart
@@ -1,19 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 import 'dart:io';
+import 'package:supabase_flutter/supabase_flutter.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:nomo/providers/supabase_provider.dart';
+import 'package:nomo/providers/saved_session_provider.dart';
 
-class ImageInput extends StatefulWidget {
+class ImageInput extends ConsumerStatefulWidget {
   const ImageInput({super.key, required this.onPickImage});
 
   final void Function(File image) onPickImage;
 
   @override
-  State<ImageInput> createState() {
+  ConsumerState<ImageInput> createState() {
     return _ImageInputState();
   }
 }
 
-class _ImageInputState extends State<ImageInput> {
+class _ImageInputState extends ConsumerState<ImageInput> {
   File? _selectedImage;
 
   void _takePicture() async {
@@ -30,8 +34,24 @@ class _ImageInputState extends State<ImageInput> {
     setState(() {
       _selectedImage = File(pickedImage.path);
     });
-
+    uploadImage(_selectedImage!);
     widget.onPickImage(_selectedImage!);
+  }
+
+  Future<void> uploadImage(File imageFile) async {
+    final supabase = ref.watch(supabaseInstance);
+    final userId = (await supabase).client.auth.currentUser!.id.toString();
+    final response = await (await supabase)
+        .client
+        .storage
+        .from('Images')
+        .upload('${userId}/anotherimage', imageFile);
+
+    // if (response.error == null) {
+    //   print('Image uploaded successfully');
+    // } else {
+    //   print('Upload error: ${response.error!.message}');
+    // }
   }
 
   @override

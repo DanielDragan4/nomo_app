eecf1ae78c0879bc542875ae703b193d2dbbc42b
DanielDragan4 <daniel.dragan4000@gmail.com>
2024-05-21 10:30:08 -0400

    bookmark starting

--- lib/providers/events_provider.dart
+++ lib/providers/events_provider.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:nomo/models/events_model.dart';
+import 'package:nomo/models/profile_model.dart';
 import 'package:nomo/providers/supabase_provider.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 
@@ -11,31 +12,29 @@ class EventProvider extends StateNotifier<List?> {
 
   Future<List> readEvents() async {
     final supabaseClient = (await supabase).client;
-    var events = await supabaseClient.from('Event').select('*, Attendees(user_id)');
+    var events = await supabaseClient.from('recommended_events').select('*, Attendees(user_id), Bookmarked(user_id)');
     return events.toList();
-  }
-  Future<List> getEventImgs() async {
-    final supabaseClient = (await supabase).client;
-    var eventImgs = await supabaseClient.from('Images').select('*');
-    return eventImgs.toList();
   }
   
   Future<void> deCodeData() async {
     final codedList = await readEvents();
-    final eventImgs = await getEventImgs();
 
     List<Event> deCodedList = [];
     final supabaseClient = (await supabase).client;
 
     for (var eventData in codedList) {
-      String url='';             
-      for(var imgData in eventImgs) {
-            if(eventData['image_id'] == imgData['images_id']) {
-              url = supabaseClient.storage
+      String profileUrl = supabaseClient.storage
                 .from('Images')
-                .getPublicUrl(imgData['image_url']);
-            }
-          }
+                .getPublicUrl(eventData['profile_path']);
+      String eventUrl = supabaseClient.storage
+                .from('Images')
+                .getPublicUrl(eventData['event_path']);
+      bool bookmarked = false;
+      for(var bookmark in eventData['Bookmarked']) {
+        if(bookmark['user_id'] == supabaseClient.auth.currentUser!.id);
+          bookmark = true;
+          break;
+      }
 
       final Event deCodedEvent = Event(
           description: eventData['description'],
@@ -44,11 +43,15 @@ class EventProvider extends StateNotifier<List?> {
           eventType: eventData['invitationType'],
           host: eventData['host'],
           imageId: eventData['image_id'],
-          imageUrl: url,
+          imageUrl: eventUrl,
           location: eventData['location'],
           title: eventData['title'],
           edate: eventData['time_end'],
-          attendees: eventData['Attendees'] 
+          attendees: eventData['Attendees'],
+          hostProfileUrl: profileUrl,
+          hostUsername: eventData['username'],
+          profileName: eventData['profile_name'],
+          bookmarked: bookmarked,
           );
 
       bool attending = false;
@@ -65,6 +68,7 @@ class EventProvider extends StateNotifier<List?> {
     }
     state = deCodedList;
   }
+
   Future<void> joinEvent(currentUser, eventToJoin) async{
     final supabaseClient = (await supabase).client;
     final newAttendeeMap = {
@@ -74,6 +78,21 @@ class EventProvider extends StateNotifier<List?> {
     await supabaseClient.from('Attendees').insert(newAttendeeMap);
     await deCodeData();
   }
+
+  Future<void> bookmark(eventToMark, currentUser) async{
+    final supabaseClient = (await supabase).client;
+    final newABookMarkMap = {
+      'user_id' : currentUser,
+      'event_id' : eventToMark
+    };
+    await supabaseClient.from('Bookmarked').insert(newABookMarkMap);
+  }
+
+  Future<void> unBookmark(eventToMark, currentUser) async{
+    final supabaseClient = (await supabase).client;
+    await supabaseClient.from('Bookmarked').delete().eq('event_id', eventToMark).eq('user_id', currentUser);
+  }
+
 }
 
 final eventsProvider = StateNotifierProvider<EventProvider, List?>((ref) {

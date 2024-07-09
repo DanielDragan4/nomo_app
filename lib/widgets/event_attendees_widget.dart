27be4d55c870f6c6058471a9cd28abd6d0bcc1ad
person5555 <danmartynovich@gmail.com>
2024-07-08 21:21:44 -0400

    wording adjustments

--- lib/widgets/event_attendees_widget.dart
+++ lib/widgets/event_attendees_widget.dart
@@ -5,7 +5,8 @@ import 'package:nomo/providers/events_provider.dart';
 import 'package:nomo/widgets/friend_tab.dart';
 
 class AttendeesSection extends ConsumerStatefulWidget {
-  const AttendeesSection({super.key, required this.eventId, required this.areFriends});
+  const AttendeesSection(
+      {super.key, required this.eventId, required this.areFriends});
 
   final eventId;
   final bool areFriends;
@@ -24,14 +25,17 @@ class _AttendeesSectionState extends ConsumerState<AttendeesSection> {
   }
 
   Future<void> receiveComments() async {
-    if(widget.areFriends) {
-      var readEventAttendees = await ref.read(eventsProvider.notifier).getEventFriends(widget.eventId);
+    if (widget.areFriends) {
+      var readEventAttendees = await ref
+          .read(eventsProvider.notifier)
+          .getEventFriends(widget.eventId);
       setState(() {
         attendeesList = readEventAttendees;
       });
-    }
-      else{
-        var readEventAttendees = await ref.read(eventsProvider.notifier).getEventAttendees(widget.eventId);
+    } else {
+      var readEventAttendees = await ref
+          .read(eventsProvider.notifier)
+          .getEventAttendees(widget.eventId);
       setState(() {
         attendeesList = readEventAttendees;
       });
@@ -48,19 +52,39 @@ class _AttendeesSectionState extends ConsumerState<AttendeesSection> {
                 height: MediaQuery.of(context).size.height * 0.5,
                 child: ListView.builder(
                   itemCount: attendeesList.length,
-                  itemBuilder: (context, index) => FriendTab(friendData: attendeesList[index], toggle: false, isEventAttendee: true, isRequest: false,),
+                  itemBuilder: (context, index) => FriendTab(
+                    friendData: attendeesList[index],
+                    toggle: false,
+                    isEventAttendee: true,
+                    isRequest: false,
+                  ),
                 ),
               )
             : Center(
                 child: Padding(
-                  padding: EdgeInsets.all(MediaQuery.of(context).size.height * 0.02),
-                  child: Text(
-                    "No Attendees In This event",
-                    style: TextStyle(
-                      color: Theme.of(context).colorScheme.onSecondary.withOpacity(0.6),
-                      fontSize: MediaQuery.of(context).size.width * 0.04,
-                    ),
-                  ),
+                  padding:
+                      EdgeInsets.all(MediaQuery.of(context).size.height * 0.02),
+                  child: widget.areFriends
+                      ? Text(
+                          "No Friends Attending This Event",
+                          style: TextStyle(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSecondary
+                                .withOpacity(0.6),
+                            fontSize: MediaQuery.of(context).size.width * 0.04,
+                          ),
+                        )
+                      : Text(
+                          "No Attendees In This Event",
+                          style: TextStyle(
+                            color: Theme.of(context)
+                                .colorScheme
+                                .onSecondary
+                                .withOpacity(0.6),
+                            fontSize: MediaQuery.of(context).size.width * 0.04,
+                          ),
+                        ),
                 ),
               ),
       ],
